1f242ec1b0f6ccc67b286a4ee6c6b3b33b72af8e
nancy-konadu <nancykyiadom994@gmail.com>
2023-05-18 12:38:06 -0700

    More malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * malloc_checked - Allocates memory
-  * @b: the size to allocate
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
   *
-  * Return: Nothing.
+  * Return: ...
   */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-void *p;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-p = malloc(b);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (p == NULL)
-exit(98);
+	while (s1[i])
+		i++;
 
-return (p);
-}}
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
