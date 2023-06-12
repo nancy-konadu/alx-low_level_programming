34e7960cb538ca6d72984a4d9fe80ce56b4116ec
nancy-konadu <nancykyiadom994@gmail.com>
2023-06-12 09:20:00 -0700

    File io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,44 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file
- * @filename: path of the file to read
- * @letters: number of letters to read and print
- * Return: number of letters readed.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the file to read
+ * @letters: numb of letters it should read and print
+ *
+ * Return: actual numb of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
-	int count, fd_o, fd_r;
 
 	if (filename == NULL)
 		return (0);
-
-	/* Open file and get file descriptor */
-	fd_o = open(filename, O_RDONLY);
-	if (fd_o == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	/* allocate memory in buffer to read contents of fd */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
-
-	/* read contents of fd */
-	fd_r = read(fd_o, buffer, letters);
-	if (fd_r == 0)
 	{
-		free(buffer);
-		close(fd_o);
+		close(fd);
 		return (0);
 	}
-
-	/* write to standard output */
-	count = write(STDOUT_FILENO, buffer, fd_r);
-	if (count == -1)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
 		free(buffer);
-		close(fd_r);
-		close(fd_o);
 		return (0);
 	}
-	close(fd_r);
-	close(fd_o);
-
-	return (count);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
